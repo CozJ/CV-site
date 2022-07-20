53c3724d1040c2aaa945b1a408b66693efc22811
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2022-07-20 19:00:34 +0100

    3D objects

--- src/lib/Pointfield.svelte
+++ src/lib/Pointfield.svelte
@@ -4,9 +4,13 @@
 
 </script>
 
-<div class="relative w-[70vw] h-[30vh] lg:w-[30rem] lg:h-[30rem]">
-    <a-scene embedded  vr-mode-ui="enabled: false">
-        <a-camera look-controls="enabled: false"/>
-        <a-box position="-1 0.5 -3"  rotation="0 45 0" color="#4CC3D9" width="0.2" height="0.2" depth="0.2"></a-box>
-    </a-scene>
-</div>
+    <div class="h-[36vh]  w-[80vw] lg:h-[39vw] lg:w-[39vw]">
+        <a-scene embedded vr-mode-ui="enabled: false">
+            <a-camera position="0 0 1" look-controls="enabled: false" fov="50"/>
+            <a-box animation="property: rotation; to: 0 -180 180; loop: true; dur: 5000;" position="0 0 -1"  rotation="0 0 0" color="#4CC3D9" width="0.1" height="0.1" depth="0.1"></a-box>
+            <a-tetrahedron animation="property: rotation; to: 0 180 180; loop: true; dur: 5000;" position="0.3 0.2 -0.8"  rotation="0 0 0" color="#4CC3D9" radius="0.06"></a-tetrahedron>
+            <a-torus animation="property: rotation; to: 0 180 180; loop: true; dur: 5000;" position="-0.3 -0.2 -0.8"  rotation="0 0 0" color="#4CC3D9" radius="0.06" radius-tubular="0.01" radius-ring="0.3"></a-torus>
+        </a-scene>
+    </div>
+
+    
