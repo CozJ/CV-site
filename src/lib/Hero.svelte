64f898493229456d202d708c2ff1456b2a07b4b7
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2022-08-01 15:47:40 +0100

    3d boulder addition

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -2,7 +2,7 @@
     import { scrollTo } from 'svelte-scrolling';
     import Navbar from "./Navbar.svelte";
     import KeyboardDoubleArrowDown from "svelte-material-icons/ChevronDoubleDown.svelte";
-    import Pointfield from "./Pointfield.svelte";
+    import ThreeContainer from "./ThreeContainer.svelte";
 
     let y: number = 0;
     let windowHeight: number = 0;
@@ -19,7 +19,7 @@
             <h2 class="leading-loose text-xl lg:text-3xl">Work in Progress CV site</h2>
         </div>
         <div class="flex flex-col p-10 lg:p-20 max-w-full lg:w-1/2">
-            <Pointfield/>
+            <ThreeContainer/>
         </div>
     </div>
     <div class="h-[10vh] flex items-center justify-center">
@@ -29,7 +29,7 @@
     </div>
 </div>
 {#if y > (windowHeight * 0.85)}
-    <Navbar y={y} ></Navbar>
+    <Navbar ></Navbar>
 {/if}
 
 <svelte:window  bind:scrollY={y} bind:outerHeight={windowHeight} bind:outerWidth={windowWidth}/>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,24 +5,24 @@
 
     let open: boolean = false;
 
-    export let y: number = 0;
-
     function OnClick () {
         open = !open;
     }
 </script>
 
 <div class="text-white min-w-full max-w-full bg-[#013C58] top-0 fixed h-16 z-50 flex flex-row items-center justify-between">
-    <p class="text-3xl m-4">Corey Sutcliffe {y}</p>
+    <p class="text-3xl m-4">Corey Sutcliffe</p>
     {#if open == true}
     <div class="pr-4" on:click={OnClick}>
         <MenuOpen color="white" height="48" width="48"/>
     </div>
-    <!-- <ul class="text-2xl flex flex-row">
-        <li use:scrollTo={'Skills'} class="m-4">Skills</li>
-        <li use:scrollTo={'Workplace'} class="m-4">WorkPlace</li>
-        <li use:scrollTo={'Projects'} class="m-4">Projects</li>
-    </ul> -->
+    <div class="">
+        <ul class="text-2xl flex flex-col">
+            <li use:scrollTo={'Skills'} class="m-4">Skills</li>
+            <li use:scrollTo={'Workplace'} class="m-4">WorkPlace</li>
+            <li use:scrollTo={'Projects'} class="m-4">Projects</li>
+        </ul>
+    </div>
     {:else if open == false}
     <div class="pr-4" on:click={OnClick}>
         <Menu color="white" height="48" width="48"/>
